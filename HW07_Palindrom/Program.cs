9423baaaa1d69c69529180aae2a37003077728a7
German <nemets80@mail.ru>
2022-12-10 03:14:06 +0300

    палиндром

---
+++ HW07_Palindrom/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int a = x/10000;
+int b = x%10;
+int c = (x/1000)%10;
+int d = (x%100)/10;
+if (a==b & c==d)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
